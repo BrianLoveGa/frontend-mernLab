57ae171db94d260f7a67b6af1e717ea04ee21a54
jopara94 <jopara94@gmail.com>
2019-10-16 15:26:34 -0500

    Updated and allllllmost got the button working now

--- jokesters/src/Jokeadd.js
+++ jokesters/src/Jokeadd.js
@@ -1,33 +1,47 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 export default class Jokeadd extends Component {
-	render() {
-		return (
-			<div>
-				<h2 className="about">
-					This is a story all about the add a joke page.
-					<br></br>
-					This is where you add a joke.
-					<br></br>
-					Not much else about it.
-				</h2>
-				<br></br>
-				<form>
-					<label for="id">joke id</label>
-					<br></br>
-					{/* <input> type="text" name="id" value="Joke id (a number)" </input> */}
-					<label for="setup">Setup for Joke</label>
-					<br></br>
-					{/* <input>type="text" name="setup" value=" Joke set-up "</input> */}
-					<label for="punchline">Punchline </label>
-					{/* <input>
-						{' '}
-						type="text" name="punchline"value="and the funny punchline"{' '}
-					</input>
-					<input> class="button" type="submit" value="Add your joke"</input> */}
-				</form>
-			</div>
-		);
-	}
+  constructor() {
+    super();
+    this.state = {
+      setup: "",
+      punchline: ""
+    };
+    this.myChangeHandler = this.myChangeHandler.bind(this);
+  }
+
+  myChangeHandler = event => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({ [nam]: val });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2 className="about">
+          This is a story all about the add a joke page.
+          <br></br>
+          This is where you add a joke.
+          <br></br>
+          Not much else about it.
+        </h2>
+        <br></br>
+        <form>
+          <p>Setup:</p>
+          <input type="text" name="setup" onChange={this.myChangeHandler} />
+          <p>Punchline:</p>
+          <input type="text" name="punchline" onChange={this.myChangeHandler} />
+
+          <button type="submit">Submit</button>
+
+          {/*<input type="submit" onClick={}*/}
+        </form>
+        <h1>
+          {this.state.setup} ... {this.state.punchline}
+        </h1>
+      </div>
+    );
+  }
 }
